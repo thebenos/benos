dad9b1886e0c3fe4f92717d46e2b3529b6f9aec4
Wither__ <163687438+TRWither@users.noreply.github.com>
2025-02-14 18:51:57 +0100

    Added FAT12!!!

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -10,12 +10,14 @@
 
 start:
 ; Initialize segments
-    mov ax, 0x1000
+    mov ax, 0x2000
     mov ds, ax
     mov es, ax
-    mov ax, 0x8000
+    mov ax, 0x1000
     mov ss, ax
-    mov sp, 0xf000
+    xor sp, sp
+
+    cld
 
     mov si, segInit
     call STDIO_print
@@ -68,15 +70,15 @@ shell_begin:
     je .scrollup
 
 ; Check commands
-    check_command cmdInfo, STRING_compare, .command_info
-    check_command cmdInfo_version, STRING_compare, .command_info_version
-    check_command cmdInfo_name, STRING_compare, .command_info_name
+    check_command cmdInfo, STRING_compare, command_info
+    check_command cmdInfo_version, STRING_compare, command_info_version
+    check_command cmdInfo_name, STRING_compare, command_info_name
 
-    check_command cmdHelp, STRING_compare, .command_help
-    check_command cmdHelp_info, STRING_compare, .command_help_info
-    check_command cmdHelp_halt, STRING_compare, .command_help_halt
+    check_command cmdHelp, STRING_compare, command_help
+    check_command cmdHelp_info, STRING_compare, command_help_info
+    check_command cmdHelp_halt, STRING_compare, command_help_halt
 
-    check_command cmdHalt, STRING_compare, .command_halt
+    check_command cmdHalt, STRING_compare, command_halt
 
     jmp .command_unknow
 
@@ -102,50 +104,6 @@ shell_begin:
     call VIDEO_scrollup
     
     jmp shell_begin
-.command_info:
-; Cannot use 'info' alone without arguments
-    mov si, cmdInfo_error
-    call STDIO_print
-
-    jmp shell_begin
-
-    .command_info_version:
-    ; Display the version of the system
-        mov si, SYS_VERSION
-        call STDIO_print
-        mov si, NEWLINE
-        call STDIO_print
-
-        jmp shell_begin
-    .command_info_name:
-    ; Display the name of the system
-        mov si, SYS_NAME
-        call STDIO_print
-        mov si, NEWLINE
-        call STDIO_print
-
-        jmp shell_begin
-.command_help:
-; Display a list of all the available commands
-    mov si, msgHelp_main
-    call STDIO_print
-
-    jmp shell_begin
-    .command_help_info:
-    ; Display a list of all the available options for the 'info' command
-        mov si, msgHelp_info
-        call STDIO_print
-
-        jmp shell_begin
-    .command_help_halt:
-    ; Display a list of all the available options for the 'halt' command
-        mov si, msgHelp_halt
-        call STDIO_print
-
-        jmp shell_begin
-.command_halt:
-; Stop the system
-    hlt
 
 ; ----- INCLUDES -----
 %include "lib/stdio.asm"
@@ -153,6 +111,10 @@ shell_begin:
 %include "lib/string.asm"
 %include "lib/disk.asm"
 %include "lib/general.asm"
+
+%include "kernel/help.asm"
+%include "kernel/info.asm"
+%include "kernel/halt.asm"
 
 ; ----- DATA -----
 segInit:            db      "[OK] Segments initialized", 13, 10, 0
@@ -180,22 +142,3 @@ cmdHelp_halt        db      "help halt", 0
 
 ; Commands errors
 cmdInfo_error:      db      "'info' command requires an option.", 13, 10, 'Try help info for a list of options.', 13, 10, 0
-
-; Help messages
-msgHelp_main:
-    db "HELP -- Available commands:", 13, 10
-    db "- info <-v/-n>", 13, 10
-    db "- help [command]", 13, 10
-    db "- halt", 13, 10
-    db 0
-
-msgHelp_info:
-    db "INFO -- Available options:", 13, 10
-    db "1. -v : display the version of the system", 13, 10
-    db "2. -n : display the name of the system", 13, 10
-    db 0
-
-msgHelp_halt:
-    db "HALT -- Available options:", 13, 10
-    db "No option is available for now", 13, 10
-    db 0
--- lib/disk.asm
+++ lib/disk.asm
@@ -1,4 +1,9 @@
-; This file contains several subroutines used to interact with the disk.
+; This file contains several subroutines used to interact with the disk
+; and the filesystem.
+;
+; IMPORTANT:
+; This file depends of "lib/stdio.asm", so you must include it in your
+; program too.
 
 [bits 16]
 
@@ -12,105 +17,3 @@ DISK_error:
     int 0x10
 
     jmp $
-
-DISK_read:
-    mov ah, 0x02
-    ; AL = sectors to read
-    ; DL = reader
-    ; DH = head
-    ; CH = cylinder
-    ; CL = sector
-    ; ES:BX = memory address where data is copied
-    int 0x13
-    jc DISK_error
-    ret
-
-DISK_write:
-    mov ah, 0x03
-    ; AL = sectors to write
-    ; DL = reader
-    ; DH = head
-    ; CH = cylinder
-    ; CL = sector
-    ; ES:BX = memory address where data is written
-    int 0x13
-    jc DISK_error
-    ret
-
-; Usage;
-; mov ax, <cluster_number>
-; call DISK_read_entry
-;
-; NOTE:
-; At the end, the entry is stored in BX.
-; 0xfff = EOF (End Of File)
-DISK_read_entry:
-    push ax
-    push bx
-    push si
-
-    ; AX = cluster number
-    imul ax, 3
-    mov bx, ax
-    mov si, 0x2000          ; FAT address
-    add si, bx
-
-; Read the 3 bytes of the FAT entry
-    mov al, [si]                    ; 1st byte
-    mov ah, [si + 1]                ; 2nd byte
-    mov bl, [si + 2]                ; 3d byte
-
-    shl ah, 4
-    or ah, al
-    shl bl, 8
-    or bl, ah
-
-    ret
-
-; Usage:
-; mov ax, <first_cluster>
-; mov bx, <buffer>
-; mov dl, <reader>
-; call DISK_read_file
-DISK_read_file:
-    push ax
-    push bx
-    push cx
-    push dx
-    push di
-
-    mov cx, ax
-.begin:
-    cmp cx, 0xfff            ; EOF?
-    je .done
-
-; Read current cluster
-    mov dh, 0
-    mov ch, 0
-    mov ax, cx
-    ; BX = buffer
-    push bx
-    mov es, bx
-    mov al, 1
-    call DISK_read
-    pop bx
-
-    add bx, 512
-
-; Get next cluster
-    mov ax, cx
-    call DISK_read_entry
-    mov cx, bx
-
-    cmp cx, 0x000            ; Avoid endless loop
-    je .done
-    cmp cx, 0xfff            ; EOF
-    jne .begin
-
-.done:
-    pop di
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    ret
