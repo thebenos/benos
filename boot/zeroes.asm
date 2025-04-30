65357122bd67dbc75a5c7103e56907796e2b4b64
Wither__ <163687438+TRWither@users.noreply.github.com>
2025-04-30 08:34:31 +0200

    Delete boot/zeroes.asm

--- boot/zeroes.asm
+++
@@ -1,9 +0,0 @@
-; ===================================================================
-; zeroes.asm
-;
-; Released under MIT license (see LICENSE for more informations)
-;
-; This file contains zeroes that are used to fill the disk.
-; ===================================================================
-
-times 1468594 db 0
