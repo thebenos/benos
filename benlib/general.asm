6f972f211a89edd64cada27c83be7f9385658aae
Wither__ <163687438+TRWither@users.noreply.github.com>
2025-06-23 19:06:39 +0200

    Delete benlib directory

--- benlib/general.asm
+++
@@ -1,7 +0,0 @@
-; This file contains usable general informations about BenOS.
-
-[bits 16]
-
-SYS_VERSION:            db          "0.0.8", 0  ; Current version of the system
-SYS_NAME:               db          "BenOS", 0          ; Name of the system
-SYS_LICENSE:            db          "MIT", 0
